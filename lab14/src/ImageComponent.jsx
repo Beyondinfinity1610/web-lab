c0ab9908d7b5ab3c596f91cdf7221e686091dadd
Beyondinfinity1610 <160531253+Beyondinfinity1610@users.noreply.github.com>
2025-04-13 18:46:41 +0530

    file updates

--- lab14/src/ImageComponent.jsx
+++ lab14/src/ImageComponent.jsx
@@ -3,7 +3,7 @@ import userImage from "../src/Images/pic.jpg";
 
 const ImageComponent = () => (
   <div style={{ textAlign: "center", margin: "20px 0" }}>
-    <img src="../public/images/pp.webp" alt="Public File" width="200" height="200px" style={{ marginRight: "10px" }} />
+    <img src="/images/pp.webp" alt="Public File" width="200" height="200" style={{ marginRight: "10px" }} />
     <img src={userImage} alt="Private File" width="200" />
   </div>
 );
